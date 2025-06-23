df6463f5cbbd351f190602b98fddd4ca0c85bd13
Numan Kaplan <numankaplan32@gmail.com>
2025-06-23 16:37:32 +0300

    Add files via upload

---
+++ Web.Core/PersonalICoreService/ICoreService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Web.Core.Props;
+
+namespace Web.Core.PersonalICoreService
+{
+   public interface ICoreService <T> where T : CoreEntity
+    {
+        public bool Create(T entity);
+        public bool Update(T entity);
+        public bool Delete(T entity);
+        public T GetById(int id);
+        public IEnumerable<T> GetAll();
+        public bool Save();
+    }
+}
---
+++ Web.Core/Props/CoreEntity.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web.Core.Props
+{
+  public class CoreEntity
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
